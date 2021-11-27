96c1aa492b7f2b68309f089c6dfc2611c52e5d6c
Elyor941004 <ernazarovelyor941004@gmail.com>
2021-11-27 20:53:48 +0500

    fayl

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -4,25 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Models\Films;
 use App\Models\Janr;
+use http\Url;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
 {
     protected function Janrs(){
         $model = Janr::paginate(10);
-        return response()->json(['$model'=>$model]);
+        return response()->json(['janr'=>$model]);
     }
     protected function Films($id){
         $janr = Janr::find($id);
         $model = Films::where('janr_id', $janr->id)->paginate(10);
-        return response()->json(['model'=>$model]);
+        return response()->json(['film'=>$model]);
     }
     protected function AllFilm(){
-        $model = Films::all();
-        return response()->json(['model'=>$model]);
+        $models = Films::paginate(10);;
+        return response()->json(['film'=>$model]);
     }
     protected function Film($id){
         $model = Films::where('id', $id)->paginate(10);
-        return response()->json(['model'=>$model]);
+        return response()->json(['film'=>$model]);
     }
 }
